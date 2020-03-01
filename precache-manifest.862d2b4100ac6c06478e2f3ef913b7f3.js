c3d7e624d936c3e028c44d435695293a5666051b
harish <harishbohare@gmail.com>
2020-03-01 16:20:04 +0530

    Updates

---
+++ precache-manifest.862d2b4100ac6c06478e2f3ef913b7f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abec82e8a9b92c5ea2d94ff6edbda86b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "67a78ae040247f41e5ae",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "08194936469b69e63ee4",
+    "url": "/my-app/static/js/2.bc5b117a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.bc5b117a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a78ae040247f41e5ae",
+    "url": "/my-app/static/js/main.0c893ca3.chunk.js"
+  },
+  {
+    "revision": "0c10fa8248c25a2f1677",
+    "url": "/my-app/static/js/runtime-main.dd12a99c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
